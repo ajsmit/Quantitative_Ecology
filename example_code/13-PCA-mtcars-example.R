eb79e415df956841fe1068e9a32792d47baae52d
Ubuntu <ubuntu@ip-172-31-49-237.ec2.internal>
2021-07-16 07:50:22 +0000

    Add descriptor for mtcars example.

--- example_code/13-PCA-mtcars-example.R
+++ example_code/13-PCA-mtcars-example.R
@@ -1,3 +1,9 @@
+# Quantitative Ecology (BCB743)
+# Principal Coordinate Analysis - mtcars example using prcomp() and ggbiplot()
+# Author: AJ Smit
+# Date: 16 July 2021
+
+# install.packages("devtools")
 # library(devtools)
 # Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS = "true")
 # install_github("vqv/ggbiplot")
@@ -17,7 +23,9 @@ ggbiplot(cars_pca)
 ggbiplot(cars_pca, labels = rownames(mtcars))
 
 # assign the cars to the countries within which they are used
-mtcars_country <- c(rep("Japan", 3), rep("US",4), rep("Europe", 7),rep("US",3), "Europe", rep("Japan", 3), rep("US",4), rep("Europe", 3), "US", rep("Europe", 3))
+mtcars_country <- c(rep("Japan", 3), rep("US",4), rep("Europe", 7),
+                    rep("US",3), "Europe", rep("Japan", 3), rep("US",4),
+                    rep("Europe", 3), "US", rep("Europe", 3))
 
 # more plots...
 ggbiplot(cars_pca, ellipse = TRUE, labels = rownames(mtcars),
