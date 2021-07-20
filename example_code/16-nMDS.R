74eeb58aa87a2a5d8bd63c8bd216e9f6b8fd0b43
ajsmit <albertus.smit@gmail.com>
2021-07-20 08:21:51 +0200

    Add nMDS script.

---
+++ example_code/16-nMDS.R
@@ -0,0 +1,71 @@
+# Quantitative Ecology (BCB743)
+# non-Metric Multidimensional Scaling
+# Author: AJ Smit
+# Date: 20 July 2021
+
+
+# Set-up ------------------------------------------------------------------
+
+library(tidyverse)
+library(vegan)
+
+
+# The Doubs River species data --------------------------------------------
+
+spe <- read.csv("Num_Ecol_R_book_ed1/DoubsSpe.csv")
+spe <- dplyr::select(spe, -1)
+spe <- dplyr::slice(spe, -8)
+
+
+# Do the nMDS -------------------------------------------------------------
+
+spe_nmds <- metaMDS(spe, distance = "bray")
+spe_nmds
+
+summary(spe_nmds) # not useful
+
+
+# Ordination diagrams -----------------------------------------------------
+
+par(mfrow = c(2, 2))
+stressplot(spe_nmds, main = "Shepard plot")
+ordiplot(spe_nmds, type = "t", cex = 1.5, main = paste0("nMDS stress = ", round(spe_nmds$stress, 2)))
+gof = goodness(spe_nmds)
+plot(spe_nmds, type = "t", main = "Goodness of fit")
+points(spe_nmds, display = "sites", cex = gof * 200) # bigger bubbles indicate a worse fit
+
+# buold from scratch
+pl <- ordiplot(spe_nmds, type = "none", main = "nMDS fish abundances ")
+points(pl, "sites", pch = 21, cex = 1.75, col = "grey80", bg = "grey80")
+points(pl, "species", pch = 21, col = "turquoise", arrows = TRUE)
+text(pl, "species", col = "blue4", cex = 0.9)
+text(pl, "sites", col = "red4", cex = 0.9)
+
+# make a plot with ordisurf()
+require('viridis')
+palette(viridis(8))
+par(mar = c(4, 4, 0.9, 0.5) + .1, mfrow = c(2, 2))
+with(spe, tmp <- ordisurf(spe_nmds ~ Satr, bubble = 3,
+                          family = quasipoisson, knots = 2, col = 6,
+                          display = "sites", main = "Satr"))
+abline(h = 0, v = 0, lty = 3)
+with(spe, tmp <- ordisurf(spe_nmds ~ Scer, bubble = 3,
+                          family = quasipoisson, knots = 2, col = 6,
+                          display = "sites", main = "Scer"))
+abline(h = 0, v = 0, lty = 3)
+with(spe, tmp <- ordisurf(spe_nmds ~ Teso, bubble = 3,
+                          family = quasipoisson, knots = 2, col = 6,
+                          display = "sites", main = "Teso"))
+abline(h = 0, v = 0, lty = 3)
+with(spe, tmp <- ordisurf(spe_nmds ~ Cogo, bubble = 3,
+                          family = quasipoisson, knots = 2, col = 6,
+                          display = "sites", main = "Cogo"))
+abline(h = 0, v = 0, lty = 3)
+
+env <- read.csv("Num_Ecol_R_book_ed1/DoubsEnv.csv")
+env <- dplyr::select(env, -1)
+env <- dplyr::slice(env, -8)
+
+(spe_nmds_env <- envfit(spe_nmds, env))
+plot(spe_nmds_env, col = "grey40")
+plot(spe_nmds_env, p.max = 0.05, col = "red")
